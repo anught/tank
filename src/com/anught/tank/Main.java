c4c14b0d4cc9d1ab231f84aeba0a1abf35bd7b4b
anught <782655752@qq.com>
2020-07-09 06:58:49 +0800

    add Thread

--- src/com/anught/tank/Main.java
+++ src/com/anught/tank/Main.java
@@ -3,14 +3,19 @@ package com.anught.tank;
 public class Main {
 	public static void main(String[] args) {
 		TankFrame f = new TankFrame();
-		while (true) {
-			f.repaint();
-			try {
-				Thread.sleep(50);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+		new Thread(() -> {
+			while (true) {
+				f.repaint();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-		}
+		}).start();
+		;
+
 	}
 }
