994825d33499cf43c6d331621ba7e30996deb748
Administrator <Administrator@SC-202003021003>
2020-07-08 22:39:34 +0800

    显示 刷新

--- src/com/anught/tank/Main.java
+++ src/com/anught/tank/Main.java
@@ -3,6 +3,14 @@ package com.anught.tank;
 public class Main {
 	public static void main(String[] args) {
 		TankFrame f = new TankFrame();
-
+		while (true) {
+			f.repaint();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- src/com/anught/tank/TankFrame.java
+++ src/com/anught/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.anught.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -10,10 +12,11 @@ public class TankFrame extends Frame {
 	int y = 200;
 
 	public TankFrame() {
-		setSize(800, 600);
+		setSize(800, 800);
 		setResizable(false);// 不能改变大小
 		setTitle("TANK WAR");
 		setVisible(true); // 设置可见
+		this.addKeyListener(new MyKeyListener());
 		addWindowListener(new WindowAdapter() { // WindowAdapter 实现了 WindowListener 的接口
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -22,14 +25,33 @@ public class TankFrame extends Frame {
 				System.exit(0);
 			}
 		});
+
 	}
 
 	@Override
 	public void paint(Graphics g) {// 只要窗口动，此方法会重新调用
-		x += 10;
-		y += 10;
 
 		g.fillRect(x, y, 50, 50);
-		System.out.println("ddd");
+	}
+
+	class MyKeyListener extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) { // 键盘按下 会调用此方法
+			// TODO Auto-generated method stub
+			// super.keyPressed(e);
+			System.out.println("press");
+			x += 100;
+			y += 100;
+
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) { // 松开键盘 会调用此方法
+			// TODO Auto-generated method stub
+			// super.keyReleased(e);
+			System.out.println("release");
+		}
+
 	}
 }
